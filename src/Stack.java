86e6727bd85c3072fd2ef213ae682524f59f4d35
Jude <91631@farnborough.ac.uk>
2020-01-14 16:53:46 +0000

    Initial commit

---
+++ src/Stack.java
@@ -0,0 +1,43 @@
+public class Stack {
+    private static int[] myArrStack = new int[5];
+    private static int top = -1;
+    public static void main(String[] args){
+        push(5);
+        push(12);
+        push(56);
+        showStack();
+        System.out.println(pop());
+        System.out.println(pop());
+        push(98);
+        showStack();
+        System.out.println(pop());
+    }
+
+    private static void push(int item){
+        if(top == myArrStack.length-1){
+            System.out.println("Error, Stack is full");
+        }
+        else{
+            top++;
+            myArrStack[top] = item;
+        }
+    }
+
+    private static int pop(){
+        if(top == -1){
+            System.out.println("Error, Stack is empty");
+        }
+        else{
+            int item = myArrStack[top];
+            top --;
+            return item;
+        }
+        return 0;
+    }
+
+    private static void showStack(){
+        for(int i=0; i<= top; i++){
+            System.out.println(myArrStack[i] + " ");
+        }
+    }
+}
